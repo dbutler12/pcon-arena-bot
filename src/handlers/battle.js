c737ca4ac2e7c1cd6f3d8e35507d0ffe451d9e29
Dwayne <saanail.vernath@gmail.com>
2021-05-24 16:41:17 -0700

    Updated battle function to use an object oriented approach to generating teams

--- src/handlers/battle.js
+++ src/handlers/battle.js
@@ -16,19 +16,52 @@ function battle(r_client, message, args){
 		hgetall(`char_data_${id_arr[4]}`).
 		exec(function(err,results){
 			let char_arr = [];
-			char_arr[0] = results[0];
+			let char_obj = {};
+			char_obj['name'] = results[0]['name'];
+			char_obj['position'] = results[0]['position'];
+			char_arr[0] = char_obj;
+			
 			for(let i = 1; i < results.length; i++){
-				let cur = results[i];
+				let cur = {};
+				cur['name'] = results[i]['name'];
+				cur['position'] = results[i]['position'];
+				
 				for(let j = i-1; j >= 0; j--){
 					if(char_arr[j]['position'] < cur['position']){
 						char_arr[j+1] = char_arr[j];
+						if(j === 0){
+							char_arr[j] = cur;
+						}
 					}else{
 						char_arr[j+1] = cur;
 						break;
 					}
 				}
 			}
-			
+		/*
+		//This example works for a basic response option
+		let user = m => m.author.id === message.author.id
+    message.channel.send(`Are you sure to delete all data? \`YES\` / \`NO\``).then(() => {
+      message.channel.awaitMessages(user, {
+          max: 1,
+          time: 10000,
+          errors: ['time']
+        })
+        .then(message => {
+          message = message.first()
+          if (message.content.toUpperCase() == 'YES' || message.content.toUpperCase() == 'Y') {
+            message.channel.send(`Deleted`)
+          } else if (message.content.toUpperCase() == 'NO' || message.content.toUpperCase() == 'N') {
+            message.channel.send(`Terminated`)
+          } else {
+            message.channel.send(`Terminated: Invalid Response`)
+          }
+        })
+        .catch(collected => {
+            message.channel.send('Timeout');
+        });
+    })
+			*/
 			str = "Battle Against: \n";
 			for(let i = 0; i < char_arr.length; i++){
 				str = str + i + " " + char_arr[i]['name'] + " at " + char_arr[i]['position'] + "\n";
