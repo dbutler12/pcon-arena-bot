b80441a57e701d362a3b9637e5b3891c1a0d24b6
Dwayne <saanail.vernath@gmail.com>
2021-05-24 22:12:48 -0700

    Added support for reporting teams that win

--- src/handlers/battle.js
+++ src/handlers/battle.js
@@ -39,7 +39,66 @@ function Team(data, num){
 }
 
 
-function submitFirstTeam(r_client, message, nick, d_team){
+function A_Teams(version){
+	this.version = version;
+	this.num = 0;
+	
+	this.addTeam = function(team, y_n){
+		this[num] = team;
+		this[team] = y_n;
+		this.num++;
+	}
+	
+	this.getVal = function(value, on){
+		if(on === 'ratio'){
+			return parseInt(value[0])/parseInt(value[2]);
+		}else if(on === 'y'){
+			return parseInt(value[0]);
+		}
+	}
+	
+	// example properties
+	// this[0] = Jun_Miyako_Kuka_Ilya_Hiyori
+	// this["Jun_Miyako_Kuka_Ilya_Hiyori"] = 5_12
+	
+	this.filt_str = function(on){
+		if(this.num == 1){
+			let y_n = this[this[0]].split("_");
+			let team = this[0].split("_");
+			return ":" + team.join("::") + ":  " + "YES:" + y_n[0] + " NO:" + y_n[1];
+		}else{
+			let arr = [];
+			let value = this.getVal(this[this[0]],on);
+	 		arr[0] = { team:this[0], val:value };
+	 		for(let i = 1; i < this.num; i++){
+	 			let cur_val = this.getVal(this[this[i]],on);
+				let cur = { team:this[i], val:cur_val };
+				
+				for(let j = i - 1; j >= 0; j--){
+					if(arr[j].val < cur.val){
+						arr[j + 1] = arr[j];
+						if(j === 0){
+							arr[j] = cur;
+						}
+					}else{
+						arr[j + 1] = cur;
+						break;
+					}
+				}
+			}
+		}
+		let str = "";
+		for(int i = 0; i < this.num; i++){
+			let team = arr[i].team.split("_");
+			let y_n = this[arr[i].team];
+			str = str + ":" + team.join("::") + ":  " + "YES:" + y_n[0] + " NO:" + y_n[1] + "\n";
+		}
+		return str;
+	}
+}
+
+
+function submitFirstTeam(r_client, message, nick, d_team, version){
 	let user = m => m.author.id === message.author.id
   message.channel.send(`No teams exist to defeat that team. Submit 5 units to add a new team.`).then(() => {
     message.channel.awaitMessages(user, {
@@ -48,8 +107,13 @@ function submitFirstTeam(r_client, message, nick, d_team){
         errors: ['time']
       })
       .then(message => {
+      	const PREFIX = "!";
       	message = message.first();
-      	const raw_team = message.content.trim().split(/\s+/);
+      	if(message.content.startsWith(PREFIX)){
+    		const raw_team = message.content
+    		.trim()
+    		.substring(PREFIX.length)
+    		.split(/\s+/);
 				
 				if(raw_team.length === 5) {
 					let id_arr = [];
@@ -66,15 +130,19 @@ function submitFirstTeam(r_client, message, nick, d_team){
 					hgetall(`char_data_${id_arr[2]}`).
 					hgetall(`char_data_${id_arr[3]}`).
 					hgetall(`char_data_${id_arr[4]}`).
-					get(`version`).
 					exec(function(err,results){
-						let version = results[results.length - 1];
-						let a_team = new Team(results, results.length - 1);
+						let a_team = new Team(results, results.length);
 						if(a_team.num === -1) return message.channel.send("Invalid team: can't have duplicate characters.");
 						let entry = a_team.units_str();
 						let version_entry = version + "_" + entry;
 						let team_obj = { [entry]:version , [version_entry]:"1_0" };
-						r_client.hmset(d_team.units_str(), team_obj);
+						r_client.hmset(d_team.units_str(), team_obj, function(err, reply){
+							if(err){
+								console.log(err);
+								return message.channel.send("Unknown error. Please let an admin know what you did.");
+							}
+							message.channel.send("Team added!");
+						});
 					});
 				}else{
 					message.channel.send("Invalid team.");
@@ -86,8 +154,11 @@ function submitFirstTeam(r_client, message, nick, d_team){
   })
 }
 
-
+// Add support to get a count for results
+// Add support to view all
+// Add support to view only unpopular results
 function battle(r_client, message, args){
+	let tot_cnt = 10;
 	r_client.hgetall('char_nick', function(err, nick) {
 		let id_arr = [];
 		for(let i = 0; i < args.length; i++){
@@ -103,19 +174,33 @@ function battle(r_client, message, args){
 		hgetall(`char_data_${id_arr[2]}`).
 		hgetall(`char_data_${id_arr[3]}`).
 		hgetall(`char_data_${id_arr[4]}`).
+		get('version').
 		exec(function(err,results){
-			let team = new Team(results, results.length);
+			let version = results[results.length - 1];
+			let team = new Team(results, results.length - 1);
 			if(team.num === -1) return message.channel.send("Invalid team: can't have duplicate characters.");
-			// message.channel.send(team.units_str());
 		
 			r_client.hgetall(team.units_str(), function(err, results){
 				if(results === null){ // No teams exist!
-					submitFirstTeam(r_client, message, nick, team);
-				}else{
-					let str = "";
-					for(const r in results){
-						str = str + r + " " + results[r];
+					submitFirstTeam(r_client, message, nick, team, version);
+				}else{ // Teams exist!
+					let cur_ver = new A_Team(version);
+					
+					for(const r in results){ // All the submitted teams
+						const c = r.charAt(0);
+						if(c >= '0' && c <= '9'){ // y_n data
+							continue;
+						}else{ // Version data
+							let versions = results[r].split("_");
+							if(versions[0] === version){ // Current version
+								let y_n = results[versions[0]+"_"+r]
+								cur_ver.addTeam(r,y_n);
+							}else{ // Old version, //TODO: Deal with when new version arrives
+							
+							}
+						}
 					}
+					let str = cur_ver.filt_str('y');
 					message.channel.send(str);
 				}
 			});
