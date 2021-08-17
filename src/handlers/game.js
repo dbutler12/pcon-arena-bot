6bca88a8ab74140c7f98c3ef706aecbf8f3749a1
Dwayne <saanail.vernath@gmail.com>
2021-08-16 18:19:33 -0700

    Proper asynchronous behavior added in game.js

--- src/handlers/game.js
+++ src/handlers/game.js
@@ -60,7 +60,7 @@ async function resolveFight(r_client, d_client, message){
 }
 
 
-function submitWin(r_client, d_client, message, left, right, opp_tag, win_c){
+async function submitWin(r_client, d_client, message, left, right, opp_tag, win_c){
 	let l_team = await redis_h.charsToTeam(r_client, message, left.split('_'));
 	let r_team = await redis_h.charsToTeam(r_client, message, right.split('_'));
 	let l_strs = l_team.unitsEmo(d_client);
